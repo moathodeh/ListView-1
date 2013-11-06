434d8c10f41116899682d3bc27ab549496349795
wade12 <wadetaylor@engineer.com>
2013-11-06 12:46:46 +0000

    private class ListClickHandler implements OnItemClickListener

--- ListView/src/com/wade12/listview/MainActivity.java
+++ ListView/src/com/wade12/listview/MainActivity.java
@@ -2,26 +2,38 @@ package com.wade12.listview;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	ListView listView;
+	String[] locations;
+	Context context;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		context = this;													// used in nested Class below
 		
 		listView = (ListView) findViewById(R.id.listView);
 		
-		String[] planets = this.getResources().getStringArray(R.array.locationsArray);
+		locations = this.getResources().getStringArray(R.array.locationArray);
 		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_view_row, R.id.listText, planets);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_view_row, R.id.listText, locations);
 		// ArrayAdapter takes 4 arguments - context, layout of row, to, from.
+		
 		listView.setAdapter(adapter);
+		
+		listView.setOnItemClickListener(new ListClickHandler());
 		
 	} // end method onCreate
 	
@@ -32,5 +44,19 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	} // end method onCreateOptionsMenu
+	
+	
+	private class ListClickHandler implements OnItemClickListener {
+
+		@Override
+		public void onItemClick(AdapterView<?> arg0, View view, int position, long arg3) {
+			
+			TextView listText = (TextView) view.findViewById(R.id.listText);
+			String text = listText.getText().toString();
+			Toast.makeText(context, text + " clicked at position: " + position, Toast.LENGTH_SHORT).show();
+			
+		} // end method onItemClick
+		
+	} // end Class ListClickHandler
 
 } // end Class MainActivity
