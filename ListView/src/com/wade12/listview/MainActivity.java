3d3943b404d13b9fcb580ecb7a9e4b3bac99c6b9
wade12 <wadetaylor@engineer.com>
2013-11-06 15:11:51 +0000

    HeaderView added.

--- ListView/src/com/wade12/listview/MainActivity.java
+++ ListView/src/com/wade12/listview/MainActivity.java
@@ -3,6 +3,7 @@ package com.wade12.listview;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,12 +18,15 @@ public class MainActivity extends Activity {
 	ListView listView;
 	String[] locations;
 	Context context;
+	LayoutInflater inflater;
 	
+	@SuppressWarnings("static-access")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		context = this;													// used in nested Class below
+		inflater = (LayoutInflater) this.getSystemService(context.LAYOUT_INFLATER_SERVICE);
 		
 		listView = (ListView) findViewById(R.id.listView);
 		
@@ -31,8 +35,10 @@ public class MainActivity extends Activity {
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_view_row, R.id.listText, locations);
 		// ArrayAdapter takes 4 arguments - context, layout of row, to, from.
 		
-		listView.setAdapter(adapter);
+		View header = inflater.inflate(R.layout.locations_header_view, null);
 		
+		listView.addHeaderView(header);
+		listView.setAdapter(adapter);
 		listView.setOnItemClickListener(new ListClickHandler());
 		
 	} // end method onCreate
