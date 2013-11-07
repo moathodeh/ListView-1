6ff65c5958291378cbed58bd3535a67ba978e3f4
wade12 <wadetaylor@engineer.com>
2013-11-07 11:31:18 +0000

    Custom Adapter used to change the ListView.

--- ListView/src/com/wade12/listview/MainActivity.java
+++ ListView/src/com/wade12/listview/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -25,15 +26,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		context = this;													// used in nested Class below
+		context = this;		// used in nested Class below
 		inflater = (LayoutInflater) this.getSystemService(context.LAYOUT_INFLATER_SERVICE);
 		
 		listView = (ListView) findViewById(R.id.listView);
 		
 		locations = this.getResources().getStringArray(R.array.locationArray);
 		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_view_row, R.id.listText, locations);
+		// ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_view_row, R.id.listText, locations);
 		// ArrayAdapter takes 4 arguments - context, layout of row, to, from.
+		LocationsAdapter adapter = new LocationsAdapter(this, R.layout.list_view_row, R.id.listText, locations);
 		
 		View header = inflater.inflate(R.layout.locations_header_view, null);
 		
@@ -63,6 +65,39 @@ public class MainActivity extends Activity {
 			
 		} // end method onItemClick
 		
-	} // end Class ListClickHandler
+	} // end private inner Class ListClickHandler
+	
+	
+	private class LocationsAdapter extends ArrayAdapter<String> {
+		
+		String[] locations;
+		
+		public LocationsAdapter(Context context, int resource, int textViewResourceId, String[] locations) {
+			
+			super(context, resource, textViewResourceId, locations);
+			this.locations = locations;
+		} // end constructor		
+
+		@Override
+		public View getView(int position, View view, ViewGroup parent) {
+			
+			View viewLeft = inflater.inflate(R.layout.list_view_row, parent, false);
+			View viewRight = inflater.inflate(R.layout.list_view_row_right, parent, false);
+			
+			String locationName = locations[position];
+			
+			if ( position % 2 == 0 ) {
+				((TextView) viewLeft.findViewById(R.id.listText)).setText(locationName);
+				view = viewLeft;
+			} // end if
+			else {
+				((TextView) viewRight.findViewById(R.id.listText)).setText(locationName);
+				view = viewRight;
+			} // end else
+			
+			return view;
+		} // end method getView
+		
+	} // end private inner Class LocationsAdapter
 
 } // end Class MainActivity
